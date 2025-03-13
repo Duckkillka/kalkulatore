d846b402c4d57e49f03ee8e9720d1c50350f6547
Duckkillka <forlobanovegor@gmail.com>
2025-03-13 14:29:19 +0300

    create main.py dev

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+444
 
 def print_hi(n):
     # Use a breakpoint in the code line below to debug your script.
